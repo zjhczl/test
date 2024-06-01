3181addcef5833a402383f4cbad1c43bc1d3e8c5
zjhczl <zhoujie.hczl@gamil.com>
2024-06-01 16:36:26 +0800

    add 21

---
+++ test21.py
@@ -0,0 +1,23 @@
+# 数据流压缩
+import brotli
+def compress_data(data):
+    # 使用Brotli算法压缩数据
+    compressed_data = brotli.compress(data)
+    return compressed_data
+
+def decompress_data(compressed_data):
+    # 使用Brotli算法解压缩数据
+    decompressed_data = brotli.decompress(compressed_data)
+    return decompressed_data
+
+# 测试数据
+original_data = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+print("Original data:", original_data)
+
+# 压缩数据
+compressed_data = compress_data(original_data)
+print("Compressed data:", compressed_data)
+
+# 解压缩数据
+decompressed_data = decompress_data(compressed_data)
+print("Decompressed data:", decompressed_data.decode())
